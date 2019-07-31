506d359c2b1c526c07bdd3fd16e703c11acee3e9
meschling <m.esch@fancysoftware.de>
2019-07-31 11:55:20 +0200

    fixing AbstractTimeHandler

--- src/start/AbstractTimeHandler.java
+++ src/start/AbstractTimeHandler.java
@@ -4,10 +4,18 @@ import java.util.Timer;
 
 public  abstract class AbstractTimeHandler {
 
-       protected Timer timer;
+    protected Timer timer;
 
     AbstractTimeHandler(int delary, int period){
-
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+              this.handle();
+            }
+        }, delary, period);
     }
+       
+    protected abstract void handle();
 
 }
