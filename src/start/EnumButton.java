c358bef611668926aaba6076884702ba03793f97
Angie192 <angelina2esch@gmali.com>
2019-08-01 17:55:30 +0200

    Enum datei erstelt

---
+++ src/start/EnumButton.java
@@ -0,0 +1,15 @@
+package start;
+
+public enum EnumButton {
+
+
+    // BT = Betätigt( Maus ist auf Button), NBT = nicht betätigt
+
+    EndeBT, EndeNBT, FortsetzenBT, FortsetzenNBT, HauptmenüBT, HauptmenüNBT, LadenBT, LadenNBT,
+    NeuStartBT, NeuStartNBT, OptionenBT, OptionenNBT, OptionenHauptBT, OptionenHauptNBT,
+    StartBT, StartNBT, StummBT, StummNBT
+
+}
+
+
+
